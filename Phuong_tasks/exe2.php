3a1ab9302ad68858bc3dd5f12fe86ccaf94cc021
summer9 <phuongminh249@gmail.com>
2024-01-29 11:33:09 +0200

    Update exe2.php

--- Phuong_tasks/exe2.php
+++ Phuong_tasks/exe2.php
@@ -4,21 +4,25 @@
 <br>
 <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nihil suscipit ducimus corporis nostrum iure eligendi repellendus! Dolores blanditiis dolorum, dignissimos est quis praesentium, commodi iste consequatur quidem totam excepturi.</p>
 <br>
-<h3> 3.1 Write PHP code to output the following message:</h3>
+
+<h3> This is what I have done in exercise</h3>
+<br>
+<br>
+<h6> 3.1 Write PHP code to output the following message:</h3>
     <?php
         // Use backslashes to escape double quotes or use single quotes
         echo 'Hello world! My name is "Phuong"';
     ?>
-
-<h3> 3.2 Create a PHP variable named $title and assign it the value "PHP is interesting." Then, use this variable as the content within an (heading 4) element.</h3>
+<br>
+<h6> 3.2 Create a PHP variable named $title and assign it the value "PHP is interesting." Then, use this variable as the content within an (heading 4) element.</h3>
 
     <?php
          $title = "PHP is interesting";
-        echo "<h4>$title</h4>";
+        echo "<h5>$title</h5>";
     ?>
 
-
-<h3> 3.3 Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables represent the grades of three students in the course. To display this information, create an HTML table within your PHP code. The table should be structured with columns for Serial Number (S.n.), Name, and Grade, and it should look like this</h3>
+<br>
+<h6> 3.3 Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables represent the grades of three students in the course. To display this information, create an HTML table within your PHP code. The table should be structured with columns for Serial Number (S.n.), Name, and Grade, and it should look like this</h3>
 
     <?php
         $g1 = 5;
@@ -52,8 +56,9 @@
     </tr>
 
 </table>
+<br>
 
-<h3>Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3>
+<h6>Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3>
     
 
 
