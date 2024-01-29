5ebaf2a3f38958b8dd95d01bbac3dc8674539c00
summer9 <phuongminh249@gmail.com>
2024-01-29 11:39:03 +0200

    Update header.php

--- Phuong_tasks/header.php
+++ Phuong_tasks/header.php
@@ -10,9 +10,8 @@
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;1,400;1,600&family=Montserrat:ital,wght@0,300;0,400;0,700;1,500&display=swap" rel="stylesheet">
-          
-            
-            <title>PHP- This is PHP page</title>
+        
+            <title>PHP - This is PHP page</title>
     </head>
     <body>
 
