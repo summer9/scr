0c9bf9794f8769c9016e209e44953b44a2f85dc3
summer9 <phuongminh249@gmail.com>
2024-01-29 11:45:06 +0200

    change title

--- Phuong_tasks/footer.php
+++ Phuong_tasks/footer.php
@@ -10,7 +10,7 @@
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;1,400;1,600&family=Montserrat:ital,wght@0,300;0,400;0,700;1,500&display=swap" rel="stylesheet">
-        <title>Restaurant</title>
+        <title>PHP_footer</title>
     </head>
     <body>
 
